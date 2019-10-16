b0a9f1e39417691200b6e55568b1dbe96bd39d5c
ellewilson96 <eleanorwilsonn@gmail.com>
2019-10-16 19:13:40 -0400

    update again

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -15,7 +15,7 @@ class Benefits extends Component {
             </p>
             <p>Employers can use the fact that they are genuinely supporting artists and their staff by including the program in their social media and marketing campaigns, improving your corporate repuation and creating a more creative work environment which helps attract and retain employees.
             </p>
-            <p>It is estimated that 70% of NYC artist studio space has been lost to gentrification in the last 20 years and many artists leave the city due to lack of cheap studio space. Artists such as Rothko, Warhol and Basquait would now struggle to find the kind of affordable spaces they worked in. ArtWorker aims to reverse that trend, not just with much needed free studio space, but by also financially supporting them.
+            <p>It is estimated that 70% of NYC artist studio space has been lost to gentrification in the last 20 years and many artists leave the city due to lack of cheap studio space. Artists such as Rothko, Warhol and Basquiat would now struggle to find the kind of affordable spaces they worked in. ArtWorker aims to reverse that trend, not just with much needed free studio space, but by also financially supporting them.
                 </p>
             </div>
             </div>
